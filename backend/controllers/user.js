d64135e9b705bd57adbfea4a2fbd88db612a5fe6
Puru <pulageswaran@gmail.com>
2022-02-04 10:20:33 +0100

    premier commit

---
+++ backend/controllers/user.js
@@ -0,0 +1,10 @@
+const User = require('./User');
+
+
+exports.signup = (req, res, next) => {
+
+};
+
+exports.login = (req, res, next) => {
+
+};
---
+++ backend/routes/user.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+//route user a coder
+
+
+module.exports = router;
